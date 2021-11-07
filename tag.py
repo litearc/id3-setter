293cd3099b2cf3a40ebc251bcf7a4d99abf92eff
Haisam Islam <nimbuszero@gmail.com>
2021-11-07 10:48:54 -0500

    cleanup + delete other tags

--- tag.py
+++ tag.py
@@ -1,67 +1,127 @@
 #!python
-import argparse, ast, eyed3, os, sys, shutil, re
+import argparse, ast, os, sys, shutil
 from prettytable import PrettyTable
 from colorama import Fore, Style
 from datetime import datetime
 from pathlib import Path
-from mutagen.mp3 import MP3
 from mutagen.easyid3 import EasyID3
 
-# for colored text
-blue = Fore.BLUE
-green = Fore.GREEN
-red = Fore.RED
-yellow = Fore.YELLOW
+# print info, warning, error messages nicely ...................................
+
+# colors
+cred = Fore.RED
+cblue = Fore.BLUE
+cgreen = Fore.GREEN
+cyellow = Fore.YELLOW
 nc = Style.RESET_ALL
+
+def red(t):
+    return f'{cred}{t}{nc}'
+
+def blue(t):
+    return f'{cblue}{t}{nc}'
+
+def green(t):
+    return f'{cgreen}{t}{nc}'
+
+def yellow(t):
+    return f'{cyellow}{t}{nc}'
+
+def info(m):
+    print(f'{blue("[info]:")} {m}')
+
+def warning(m):
+    print(f'{yellow("[warning]:")} {m}')
+
+def error(m):
+    print(f'{red("[error]:")} {m}')
+
+# default params ...............................................................
+
+# default id3 tags to add to tags file
+deftags = ['title', 'artist', 'album', 'tracknumber', 'genre']
+
+# id3 tags to remove from mp3s
+deltags = ['acoustid_fingerprint', 'acoustid_id', 'albumartist',
+        'albumartistsort', 'albumsort', 'arranger', 'artistsort', 'asin',
+        'author', 'barcode', 'bpm', 'catalognumber', 'compilation',
+        'composersort', 'conductor', 'copyright', 'date', 'discnumber',
+        'discsubtitle', 'encodedby', 'isrc', 'language', 'length', 'lyricist',
+        'media', 'mood', 'musicbrainz_albumartistid', 'musicbrainz_albumid',
+        'musicbrainz_albumstatus', 'musicbrainz_albumtype',
+        'musicbrainz_artistid', 'musicbrainz_discid',
+        'musicbrainz_releasegroupid', 'musicbrainz_releasetrackid',
+        'musicbrainz_trackid', 'musicbrainz_trmid', 'musicbrainz_workid',
+        'musicip_fingerprint', 'musicip_puid', 'organization', 'originaldate',
+        'performer', 'performer:*', 'releasecountry', 'replaygain_*_gain',
+        'replaygain_*_peak', 'titlesort', 'version', 'website']
+
+# parse command-line arguments .................................................
 
 # https://realpython.com/command-line-interfaces-python-argparse
 p = argparse.ArgumentParser(
         description='Set id3 tags of mp3 files, and copy to directory')
-p.add_argument('action', metavar='action', type=str,
+
+p.add_argument('action', metavar='action',
         help='action to perform, valid options: {get, set}')
-p.add_argument('mp3s', metavar='mp3s', type=str, nargs='*',
-        help='mp3 files to set the id3 tag for')
-p.add_argument('-t', '--tags', dest='tags',
-        action='store', type=str, required=False,
-        default=['title', 'artist', 'album', 'discnumber', 'tracknumber', 'genre', 'composer'],
+
+p.add_argument('mp3s', metavar='mp3s', nargs='*',
+        help='mp3 files to set the id3 tags for')
+
+p.add_argument('-t', '--tags', dest='tags', default=deftags,
         help='id3 tags to set (see mutagen easyid3 tags for valid values')
+
+p.add_argument('-d', '--delete', dest='delete', default=deltags,
+        help='id3 tags to delete (see mutagen easyid3 tags for valid values')
+
 p.add_argument('-f', '--tagsfile', dest='tagsfile',
-        action='store', type=str, required=False,
-        default='/tmp/id3_tags.txt',
-        help='file to store/read id3 tags')
+        default='/tmp/id3_tags.txt', help='file to store/read id3 tags')
+
 p.add_argument('-r', '--remove', action='store_true',
         help='remove files if present')
-p.add_argument('-o', '--outdir', dest='outdir',
-        action='store', type=str, required=False,
-        default='',
-        help='directory to save tagged mp3 files to (only needed for action = get)')
+
+p.add_argument('-i', '--ignore', dest='ignore',
+        action='store_true', help='ignore non-mp3 files')
+
+p.add_argument('-o', '--outdir', dest='outdir', default='',
+        help='directory to save mp3 files to (only needed for action = get)')
+
+p.add_argument('-v', '--verbosity', dest='verbosity', default=2,
+        help='verbosity level (0: +errors, 1: +warnings, 2: +info)')
 
 args = p.parse_args()
 action = args.action
 mp3s = args.mp3s
 tags = args.tags
+delete = args.delete
 tagsfile = args.tagsfile
+ignore = args.ignore
 remove = args.remove
 outdir = args.outdir
+verbosity = int(args.verbosity)
 
 if action == 'get' and len(mp3s) == 0:
-    print(f"{red}error:{nc} need to supply mp3s")
+    error(f'need to supply mp3s')
 
 # if output directory not provided, set based on current date/time
 if outdir == '':
     now = datetime.now()
     outdir = now.strftime('output-%y%m%d-%H%M%S')
 
+# "get" action .................................................................
+
 # create table with id3 tags info for each mp3 file in `mp3s`, and write to file
 def get_tags():
     # check if tags file already exists
     if os.path.isfile(tagsfile):
         if remove:
-            print(f"found tags file {blue}{tagsfile}{nc}. removing...")
-            os.remove(tagsfile)
+            if verbosity >= 2:
+                info(f'found tags file {green(tagsfile)}. removing...')
+                os.remove(tagsfile)
         else:
-            print(f"{red}error:{nc} tags file {blue}{tagsfile}{nc} already exists. "
-                "remove file or set {blue}-r{nc} flag. exiting...")
+            if verbosity >= 0:
+                error(f'tags file {green(tagsfile)} already exists. '
+                    f'remove file or set {yellow("-r")} flag. exiting...')
             sys.exit()
 
     x = PrettyTable()
@@ -73,32 +133,33 @@ def get_tags():
         for root, _, files in os.walk(p):
             for name in files:
                 f = os.path.join(root, name)
+                r = repr(f)
                 # for non-mp3 files, simply add filename columns
-                if os.path.splitext(name)[-1] != '.mp3':
-                    row = [f,f] + ['' for _ in range(len(tags))]
+                if os.path.splitext(name)[-1] != '.mp3' and not ignore:
+                    row = [r,r] + ['' for _ in range(len(tags))]
                 else:
                     try:
                         e = EasyID3(f)
-                        row = [f,f] + [(repr(e[field] if len(e[field])>1 else e[field][0]) if field in e else '') for field in tags]
+                        row = [r,r] + [(repr(e[field] if len(e[field])>1 else
+                            e[field][0]) if field in e else '') for field in tags]
                         x.add_row(row)
-                    except BaseException:
-                        print(f"{red}error:{nc} could not read id3 tag for file {blue}{f}{nc}. skipping...")
+                    except BaseException as e:
+                        if verbosity >= 1:
+                            warning(f'could not read id3 tags for {green(f)}. skipping...')
 
     s = x.get_string(sortby='filename')
     with open(tagsfile, 'w') as f:
         f.write(s)
-        print(f"generated tags file: {green}{tagsfile}{nc}")
+        if verbosity >= 2:
+            info(f'generated tags file: {green(tagsfile)}')
+
+# "set" action .................................................................
 
 # use tags file to copy, rename, and set id3 tags for each mp3 file
 def set_tags():
     # create output directory if not present
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
-
-    # if output directory is not empty, don't continue
-    # if os.listdir(outdir):
-    #     print(f"{red}error:{nc} output directory {outdir} is not empty! exiting...")
-    #     sys.exit()
 
     f = open(tagsfile, 'r')
     f.readline() # discard first line
@@ -111,7 +172,7 @@ def set_tags():
         sections = f.readline().split('|')[1:-1]
         if not sections: break # last line (bottom border of table)
         vals = [s.strip() for s in sections]
-        orig, copy = vals[iorig], vals[icopy]
+        orig, copy = ast.literal_eval(vals[iorig]), ast.literal_eval(vals[icopy])
         copypath = os.path.join(outdir, copy)
         # check that file to copy is in output directory
         if Path(outdir) in Path(copypath).parents:
@@ -119,23 +180,25 @@ def set_tags():
             copydir = os.path.dirname(copypath)
             os.makedirs(copydir, exist_ok=True)
             if not remove and os.path.isfile(copypath):
-                print(f"{yellow}warning:{nc} file {blue}{copypath}{nc} already exists. "
-                    "remove file or set {blue}-r{nc} flag. skipping...")
+                if verbosity >= 1:
+                    warning(f'{green(copypath)} already exists. '
+                        f'remove file or set {yellow("-r")} flag. skipping...')
                 continue
-            print(f"{blue}info:{nc} processing file {blue}{copypath}{nc}")
             shutil.copy(orig, copypath)
             if os.path.splitext(copy)[-1] != '.mp3':
                 continue
-            # set id3 tags
-            # e = eyed3.load(copypath)
             e = EasyID3(copypath)
             for i,val in enumerate(vals):
                 if i != iorig and i != icopy:
                     col = columns[i]
                     e[col] = '' if val == '' else ast.literal_eval(val)
+            for key in delete:
+                if key in e:
+                    del e[key]
             e.save()
         else:
-            print(f"{red}error:{nc} output file {blue}{copypath}{nc} must be in output directory {blue}{outdir}{nc}. skipping...")
+            if verbosity >= 1:
+                warning(f'output file {green(copypath)} must be in output directory {green(outdir)}. skipping...')
 
 if action == 'get': get_tags()
 if action == 'set': set_tags()
